2259a3a940723c810af3cf26c15c06f7d5afd053
asma-ab <abichouasmaa@gmail.com>
2024-01-25 16:17:40 +0100

    add callback function to handle not found page

--- index.php
+++ index.php
@@ -4,3 +4,11 @@ require_once __DIR__ . "/vendor/autoload.php";
 use App\Router;
 
 $router = new Router();
+
+$router->get('/' , function(){
+    echo 'Home Page ';
+});
+$router->get('/about' , function(){
+    echo 'About Page ';
+});
+$router->run();
--- src/Router.php
+++ src/Router.php
@@ -4,14 +4,52 @@ namespace App;
 
 class Router
 {
-    public function get(){
+    private $handlers= [];
+    private $notFoundHandler
+    const METHOD_POST = 'POST';
+    const METHOD_GET = 'GET';
+    public function get($path, $handler){
+        $this->addHandler(self::METHOD_GET, $path, $handler);
+    }
+    public function post($path, $handler){
+
+        $this->addHandler(self::METHOD_POST, $path, $handler);
+    }
+    public function addNotFoundHandler($handler)
+    {
+        $this->notFoundHandler = $handler;
 
     }
-    public function post(){
+    public function addHandler($method, $path, $handler)
+    {
+        $this->handlers[$method . $path] = [
+            'path'=> $path,
+            'method' => $method,
+            'handler' => $handler
+    ];
 
     }
-    public function run(){
+    public function run()
+    {
+        $requestUri = parse_url($_SERVER['REQUEST_URI']);
+        $requestPath = $requestUri['path'];
+        $method = $_SERVER['REQUEST_METHOD'];
 
+        $callback = null;
+        foreach ($this->handlers as $handler) {
+            if($handler['path'] === $requestPath && $method === $handler['method']){
+                $callback = $handler['handler'];
+            }
+        }
+
+        if(!$callback){
+            header("HTTP/1.0 Not Found");
+            return;
+        }
+
+        call_user_func_array($callback, [
+            array_merge($_GET,  $_POST)
+        ]);
     }
 
 }
